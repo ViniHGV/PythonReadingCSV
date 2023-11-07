30aad4909d5d17e046d39386e554921801c29301
vinihgv <ixxvinicius@gmail.com>
2023-11-07 18:04:40 -0300

    Adicionando Lógica de inserção

--- pythonReadingDate.py
+++ pythonReadingDate.py
@@ -3,41 +3,46 @@ import sqlite3
 connector = sqlite3.connect("Loja.db")
 cursor = connector.cursor()
 
-CODREPRES = []
-TIPOPESS = []
-NOMEFAN = []
-COMISSAOBASE = []
+CodRrepres = []
+TipoPess = []
+NomeFan = []
+ComissaoBase = []
+i = 0
 
-arq=open("Repres.CSV", "r")
-s=arq.readline().rstrip()
-while s!= "":
-    s = s.split(';')
-    CODREPRES.append(int(s[0]))
-    TIPOPESS.append(str(s[1]))
-    NOMEFAN.append(str(s[2]))
-    COMISSAOBASE.append(float(s[3]))
-    s = arq.readline().rstrip()
+arq = open("Repres.CSV", "r")
+s = arq.readline().rstrip()
+while s != '':
+    s = s.split(';')    
+    CodRrepres.append(s[0])
+    TipoPess.append(str(s[1]))
+    NomeFan.append(str(s[2]))
+    ComissaoBase.append(s[3])
+    # CodRrepres.append(int(s[0]))
+    # TipoPess.append(str(s[1]))
+    # NomeFan.append(str(s[2]))
+    # ComissaoBase.append(float(s[3]))
+    s = arq.readline().rstrip()    
 arq.close
 
-sql = '''
-    CREATE TABLE IF NOT EXISTS Repres
-    (
-    CODREPRES integer primary key,
-    TIPOPESS string,
-    NOMEFAN stirng,
-    COMISSAOBASE numeric
-    )
-'''
-cursor.execute(sql)
+# sql = '''
+#     CREATE TABLE IF NOT EXISTS Repres
+#     (
+#     CODREPRES integer primary key,
+#     TIPOPESS string,
+#     NOMEFAN stirng,
+#     COMISSAOBASE numeric
+#     )
+# '''
+# cursor.execute(sql)
 
-print("Tabela Repres Criada 🚀")
+# print("Tabela Repres Criada 🚀")
 
-for i in range(len(CODREPRES)):
-    sql = f'''
-        INSERT INTO Repres (CODREPRES, TIPOPESS, NOMEFAN, COMISSAOBASE)
-        VALUES ({CODREPRES[i]}, '{TIPOPESS[i]}', '{NOMEFAN[i]}', {COMISSAOBASE[i]})
-    '''
-    cursor.execute(sql)
+# for i in range(len(CODREPRES)):
+#     sql = f'''
+#         INSERT INTO Repres (CODREPRES, TIPOPESS, NOMEFAN, COMISSAOBASE)
+#         VALUES ({CODREPRES[i]}, '{TIPOPESS[i]}', '{NOMEFAN[i]}', {COMISSAOBASE[i]})
+#     '''
+    # cursor.execute(sql)
 
 print("Valores Adicionados com Sucesso!")
 
