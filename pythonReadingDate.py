1e4d155d42f25135f1040f1106ab40a99e39e008
ViniHGV <ixxvinicius@gmail.com>
2023-11-08 10:27:07 -0300

    Adicionando Lógica para Arquivo FornClien

--- pythonReadingDate.py
+++ pythonReadingDate.py
@@ -44,6 +44,63 @@ for i in range(len(CodRrepres)):
 
 print("Valores Adicionados com Sucesso!")
 
+i = 0
+
+CODCLIFOR = []
+TIPOCF = []
+CODREPRES = []
+NOMEFAN = []
+CIDADE = []
+UF = []
+CODMUNICIPIO = []
+TIPOPESSOA = []
+COBRBANC = []
+PRAZOPGTO = []
+
+arq = open("FornClien.CSV")
+s = arq.readline().rstrip()
+while s != "":
+    s = s.split(";")
+    if i > 0:
+        CODCLIFOR.append(float(s[0]))
+        TIPOCF.append(int(s[1]))
+        CODREPRES.append(str(s[2]))
+        NOMEFAN.append(str(s[3]))
+        CIDADE.append(str(s[4]))
+        UF.append(str(s[5]))
+        CODMUNICIPIO.append(str(s[6]))
+        TIPOPESSOA.append(int(s[7]))
+        COBRBANC.append(int(s[8]))
+        PRAZOPGTO.append(str(s[9]))
+    s = arq.readline().rstrip()
+    i += 1
+arq.close
+
+sql = """
+    CREATE TABLE IF NOT EXISTS FornClien
+    (
+    CODCLIFOR numeric primary key,
+    TIPOCF integer,
+    CODREPRES string,
+    NOMEFAN string,
+    CIDADE string,
+    UF string,
+    CODMUNICIPIO string,
+    TIPOPESSOA integer,
+    COBRBANC integer,
+    PRAZOPGTO string
+    )
+"""
+cursor.execute(sql)
+
+for i in range(len(CODCLIFOR)):
+    sql = f"""
+        INSERT INTO FornClien (CODCLIFOR, TIPOCF, CODREPRES, NOMEFAN, CIDADE, UF, CODMUNICIPIO, TIPOPESSOA, COBRBANC, PRAZOPGTO)
+        VALUES ({CODCLIFOR[i]}, {TIPOCF[i]}, '{CODREPRES[i]}', '{NOMEFAN[i]}', '{CIDADE[i]}', '{UF[i]}', '{CODMUNICIPIO[i]}', {TIPOPESSOA[i]}, {COBRBANC[i]}, '{PRAZOPGTO[i]}')
+    """
+    cursor.execute(sql)
+
+print("Valores Adicionados com Sucesso!")
 
 connector.commit()
 cursor.close
